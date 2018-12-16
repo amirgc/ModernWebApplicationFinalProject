ffde6b9583b1e3dc9d58c034c195195bd2e7abb9
amirgc <gcamir15@gmail.com>
2018-12-16 09:08:45 -0600

    added other required component

---
+++ client/frontapp/src/app/user/menutab/menutab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menutab',
+  templateUrl: './menutab.component.html',
+  styleUrls: ['./menutab.component.scss']
+})
+export class MenutabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/frontapp/src/app/user/user.module.ts
+++ client/frontapp/src/app/user/user.module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from "@angular/common";
 import { UsermainComponent } from "./usermain/usermain.component";
 import { HomeComponent } from "./home/home.component";
 import { RouterModule } from "@angular/router";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { SidepanelComponent } from "./sidepanel/sidepanel.component";
+import { MenutabComponent } from "./menutab/menutab.component";
 
 @NgModule({
-  declarations: [UsermainComponent, HomeComponent],
+  declarations: [
+    UsermainComponent,
+    HomeComponent,
+    ToolbarComponent,
+    DashboardComponent,
+    SidepanelComponent,
+    MenutabComponent
+  ],
   imports: [CommonModule, RouterModule]
 })
 export class UserModule {}
