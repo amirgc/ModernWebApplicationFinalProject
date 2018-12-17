e70b9d6c1ecb988089774db3d3e47694bae909e5
amirgc <gcamir15@gmail.com>
2018-12-17 13:11:08 -0600

    item component css work out

--- client/frontapp/src/app/user/item-select-popup/item-select-popup.component.ts
+++ client/frontapp/src/app/user/item-select-popup/item-select-popup.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { OrderLine } from "./../../_models/order.model";
+import { DishListService } from "../menutab/dish-list.service";
 
 @Component({
   selector: "app-item-select-popup",
@@ -6,7 +9,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./item-select-popup.component.scss"]
 })
 export class ItemSelectPopupComponent implements OnInit {
-  constructor() {}
+  message: string;
+  selectedItemType: any;
+  selectedItemSize: any;
+  selectedDish: any;
+  itemimage: string;
+  qty: number;
+  ingredients: any[] = [];
+  ingredientsDisplayText: string;
+  description: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<ItemSelectPopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dishListService: DishListService
+  ) {
+    this.qty = 1;
+    console.log(JSON.stringify(data));
+    this.selectedDish = data.item;
+    this.itemimage = data.item.image;
+    this.description = data.item.description;
+    // These Item Type and Size is bounded with front end
+    this.selectedItemType = this.selectedDish.types[0];
+    this.selectedItemSize = this.selectedItemType.sizes[0];
+    this.getIngredients();
+  }
+
+  dishChanges(event) {
+    this.selectedItemType = event.value;
+    this.selectedItemSize = this.selectedItemType.sizes[0];
+    this.getIngredients();
+  }
+
+  getIngredients() {}
 
   ngOnInit() {}
+
+  changeQuantity(i) {
+    if (this.qty === 0 && i === -1) {
+      this.qty = 0;
+    } else {
+      this.qty = this.qty + i;
+    }
+  }
+
+  addItemToCart() {
+    this.dialogRef.close("");
+  }
 }
